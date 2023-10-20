48df39c058149d4c97d9000b1aea17a294ec298d
Daniel Wilson <danielvillenawilson@gmail.com>
2023-10-20 17:54:07 +0200

    change(add deleteFinances)

--- src/component/InfoFinances/InfoFinances.jsx
+++ src/component/InfoFinances/InfoFinances.jsx
@@ -9,6 +9,41 @@ const InfoFinances = ({ getFinance }) => {
 	const { infoFinances, setInfoFinances, infoUpdatesFinances, setInfoUpdatesFinances, resultUpdateSimu, setResultUpdateSimu } = useContext(ConsultClientContext)
 
 	const dialogEditFinances = useRef()
+	const dialogDeleteFinances = useRef()
+
+	const handleDeleteFinances = () => {
+		dialogDeleteFinances.current.showModal();
+	}
+	const handleCancelDeleteModalClient = () => {
+		dialogDeleteFinances.current.close();
+	}
+
+
+	const deleteClient = async () => {
+		try {
+			const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/finances/deleteFinances/${getFinance._id}`,
+				{
+					method: "DELETE",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						userDni: getFinance.userDni
+					}),
+				}
+			);
+			const data = await response.json();
+
+			if (data.status === "OK") {
+				toast.success("La eliminación de los datos financieros del cliente ha sido todo un éxito")
+				 setInfoFinances(data.getAllFinances) 
+				dialogDeleteFinances.current.close();
+			}
+
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	const handleModifyFinances = () => {
 		dialogEditFinances.current.showModal();
@@ -51,8 +86,6 @@ const InfoFinances = ({ getFinance }) => {
 				setInfoFinances(data.getAllFinances)
 				dialogEditFinances.current.close();
 			}
-
-
 
 		} catch (error) {
 			console.log(error);
@@ -97,12 +130,27 @@ const InfoFinances = ({ getFinance }) => {
 				</div>
 				<div className='flex'>
 					<div className="bg-red-500 w-[10%]">
-						<button className="bg-redBrown text-white w-[100%]" >Borrar</button>
+						<button className="bg-redBrown text-white w-[100%]" onClick={handleDeleteFinances} >Borrar</button>
 					</div>
 					<div className="bg-red-500 w-[10%]">
 						<button className="bg-gold text-white w-[100%]" onClick={handleModifyFinances}>Editar</button>
 					</div>
 				</div>
+				<dialog ref={dialogDeleteFinances}>
+					<div className="flex flex-col bg-backgroundSky  h-[100%] w-[50vw] h-[40vh]">
+						<div className="flex flex-col justify-around h-[80%]">
+							<div className="flex justify-center">
+								<span className="font-black text-white text-[36px]">¿Estás seguro que deseas eliminar este dato financiero del cliente?</span>
+							</div>
+							<div className="flex justify-center">
+								<div className="flex justify-around w-[65%]">
+									<button className="bg-redBrown text-white w-[15%]" onClick={handleCancelDeleteModalClient} >Cancelar</button>
+									<button className="bg-gold text-white w-[15%]" onClick={deleteClient}>Aceptar</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</dialog>
 				<dialog ref={dialogEditFinances}>
 					<form onSubmit={handleSubmitFinances}>
 						<div className="flex flex-col justify-around bg-backgroundSky  h-[100%] w-[50vw] h-[40vh]">
